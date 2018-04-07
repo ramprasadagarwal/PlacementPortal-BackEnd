1b22f0082b29c6dff9f333dce161bbcfa694afa6
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-04-08 00:13:03 +0530

    fullname add in signup

--- src/utils/helpers/encryptUserData.js
+++ src/utils/helpers/encryptUserData.js
@@ -2,6 +2,7 @@ const PasswordHash = require('password-hash');
 
 const encryptedUserData = formData => ({
   usn: formData.usn,
+  fullname: formData.fullName,
   password: PasswordHash.generate(formData.password),
 });
 
