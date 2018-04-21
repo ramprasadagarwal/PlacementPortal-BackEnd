b467c1004024a0767c887fba8a3aa6fd9aeff04f
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-04-21 09:55:46 +0530

    cgpa return nan bug fix

--- src/utils/helpers/calculateCGPA.js
+++ src/utils/helpers/calculateCGPA.js
@@ -1,20 +1,23 @@
-const calculateCGPA = details => ((
-  (details.cpga1 * details.credit1) +
-    (details.cpga2 * details.credit2) +
-    (details.cpga3 * details.credit3) +
-    (details.cpga4 * details.credit4) +
-    (details.cpga5 * details.credit5) +
-    (details.cpga6 * details.credit6) +
-    (details.cpga7 * details.credit7) +
-    (details.cpga8 * details.credit8)) /
-    (details.cgpa1 +
-      details.cgpa2 +
-      details.cgpa3 +
-      details.cgpa4 +
-      details.cgpa5 +
-      details.cgpa6 +
-      details.cgpa7 +
-      details.cgpa8
+const calculateCGPA = (details) => {
+  console.log('hey', Number(details.cgpa1));
+  return ((
+    (Number(details.cgpa1) * Number(details.credit1)) +
+    (Number(details.cgpa2) * Number(details.credit2)) +
+    (Number(details.cgpa3) * Number(details.credit3)) +
+    (Number(details.cgpa4) * Number(details.credit4)) +
+    (Number(details.cgpa5) * Number(details.credit5)) +
+    (Number(details.cgpa6) * Number(details.credit6)) +
+    (Number(details.cgpa7) * Number(details.credit7)) +
+    (Number(details.cgpa8) * Number(details.credit8))) +
+    (Number(details.cgpa1) +
+      Number(details.cgpa2) +
+      Number(details.cgpa3) +
+      Number(details.cgpa4) +
+      Number(details.cgpa5) +
+      Number(details.cgpa6) +
+      Number(details.cgpa7) +
+      Number(details.cgpa8)
     ));
+};
 
 module.exports = calculateCGPA;
